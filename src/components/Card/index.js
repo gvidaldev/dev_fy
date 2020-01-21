e766f28cabe60cf04874a3b31b9fe33903cd579d
Gabriel Vidal <gvidal.dev@gmail.com>
2020-01-20 23:52:38 -0200

    Dividindo em Components e incializando estilização.

---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Label } from './styles';
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label color="#7159c1" />
+      </header>
+      <p>Fazer migração de servidor</p>
+      <img src="https://avatars3.githubusercontent.com/u/58925352?s=460&v=4" alt=""/>
+    </Container>
+  );
+}
